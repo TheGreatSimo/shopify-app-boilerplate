36d0d6ccd310e34a87f09f73cbff4dd94b375a79
TheGreatSimo <aissaouicoder@gmail.com>
2024-10-10 05:13:08 +0100

    Group webhooks in a separate directory

---
+++ app/routes/webhooks+/app.uninstalled.tsx
@@ -0,0 +1,15 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../../shopify.server";
+import db from "../../db.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { shop, session, topic } = await authenticate.webhook(request);
+
+  console.log("uninstalled works just fine")
+  console.log("but it should return something you know")
+  console.log(`Received ${topic} webhook for ${shop}`);
+
+  // Webhook requests can trigger multiple times and after an app has already been uninstalled.
+  // If this webhook already ran, the session may have been deleted previously.
+  return new Response("Success", { status: 200 });
+};
